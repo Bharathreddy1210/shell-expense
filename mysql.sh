73f8360e13281f75e31f154d460412eea7ab3bf4
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-08 15:01:03 +0200

    mysql server

--- mysql.sh
+++ mysql.sh
@@ -9,6 +9,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "please enter the DB password::"
+read mysql_root_password:: 
 
 VALIDATE() {
         if [ $1 -ne 0 ]
@@ -42,10 +44,10 @@ VALIDATE $? "Starting mysql server"
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
 
-mysql -h db.bharathdevops.site -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.bharathdevops.site -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass-ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "mysql root password setup"
 else
     echo -e  "password already setup....$G SKIP $N"
