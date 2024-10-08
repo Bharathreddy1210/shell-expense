5c94845ae9ba5f5679d7d24e4d360f1e15b1934e
Bharathreddy1210 <bharath1210.g@gmail.com>
2024-10-08 14:07:19 +0200

    mysql server

--- mysql.sh
+++ mysql.sh
@@ -11,13 +11,13 @@ Y="\e[33m"
 N="\e[0m"
 
 VALIDATE() {
-        if [ $1 -ne 0 ] 
-      then 
-          echo -e "$2... $R FAILURE $N"
-          Exit 1
-      else
-          echo -e "$2... $G SUCCESS $N"
-        fi
+    if [ $1 -ne 0 ] 
+    then 
+        echo -e "$2... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2... $G SUCCESS $N"
+    fi
 }
 
 
@@ -37,7 +37,7 @@ systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "Enable mysql server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting mysqll server"
+VALIDATE $? "Starting mysql server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 VALIDATE $? "Setting up root password"
